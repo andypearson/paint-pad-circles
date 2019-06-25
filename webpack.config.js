91ffad90e8fcb2f0b8ea0b6c39783f6a6bca5957
Andy Pearson <andy@andy-pearson.com>
2019-06-25 09:22:06 +0100

    Install React

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,16 @@ module.exports = {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   }
 };
